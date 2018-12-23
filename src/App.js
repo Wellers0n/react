28601ccc99aaf53cf0541035a02c32771296ba31
Wellerson <wellers0n_@hotmail.com>
2018-12-23 01:00:23 -0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Link, Route} from 'react-router-dom';
 
-class App extends Component {
+class App extends Component{
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Route path='/' exact component={Home}/>
+          <Route path='/contatos' component={Contatos}/>
+          <Route path='/sobre' component={Sobre}/>
+          <Link to="/" children="Home"/><br/>
+          <Link to="/contatos" children="Contatos"/><br/>
+          <Link to="/sobre" children="Sobre"/>
+        </div>
+      </BrowserRouter>
     );
   }
+}
+
+const Home = () => {
+    return(
+      <h1>Testing pag home</h1> 
+    )
+}
+
+const Sobre = () => {
+    return(
+      <h1>Testing pag sobre</h1> 
+    )
+}
+
+const Contatos = () => {
+    return(
+      <h1>Testing pag contatos</h1> 
+    )
 }
 
 export default App;
